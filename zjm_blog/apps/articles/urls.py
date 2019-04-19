7663e3d8bc92628ef2113cfcda971997ff46bf3f
wangsy <15892766524@163.com>
2019-04-19 12:44:37 +0800

    上线

--- zjm_blog/apps/articles/urls.py
+++ zjm_blog/apps/articles/urls.py
@@ -15,5 +15,5 @@ from .views import Articles, CatArticlesList, AboutMe
 urlpatterns = [
     url(r'article/(?P<a_id>\d+)', Articles.as_view(), name='info'),
     url(r'category/(?P<cat_id>\d+)', CatArticlesList.as_view(), name='list'),
-    url(r'about/', AboutMe.as_view(), name='about')
+    # url(r'about/', AboutMe.as_view(), name='about')
 ]
